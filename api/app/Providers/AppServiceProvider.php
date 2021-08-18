17e4ce7f88862c324260f9472b4ac84a73b30b10
foo <foo.avoutar7654@gmail.com>
2021-08-18 00:14:20 +0000

    [chore]サービスコンテナのセットアップ

--- api/app/Providers/AppServiceProvider.php
+++ api/app/Providers/AppServiceProvider.php
@@ -2,7 +2,25 @@
 
 namespace App\Providers;
 
+use App\Infrastructure\LaravelWebHookServer;
+use App\Repositories\RdbmsActivityRepository;
+use App\Repositories\RdbmsClanBattleRepository;
+use App\Repositories\RdbmsClanRepository;
+use App\Repositories\RdbmsMemberRepository;
+use App\Repositories\RdbmsReportChannelRepository;
+use App\Repositories\RdbmsWebHookRepository;
+use App\Support\MessageResolverFromConfig;
+use App\Support\UlidGenerator;
 use Illuminate\Support\ServiceProvider;
+use Sylph\Application\Gateway\WebHookServer;
+use Sylph\Application\Support\MessageResolver;
+use Sylph\Application\Support\UlidGenerator as UlidGeneratorInterface;
+use Sylph\Repositories\ActivityRepository;
+use Sylph\Repositories\ClanBattleRepository;
+use Sylph\Repositories\ClanRepository;
+use Sylph\Repositories\MemberRepository;
+use Sylph\Repositories\ReportChannelRepository;
+use Sylph\Repositories\WebHookRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +33,21 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    public array $bindings = [
+        // Repositories
+        ClanRepository::class => RdbmsClanRepository::class,
+        ClanBattleRepository::class => RdbmsClanBattleRepository::class,
+        ReportChannelRepository::class => RdbmsReportChannelRepository::class,
+        WebHookRepository::class => RdbmsWebHookRepository::class,
+        MemberRepository::class => RdbmsMemberRepository::class,
+        ActivityRepository::class => RdbmsActivityRepository::class,
+
+        // Supports
+        UlidGeneratorInterface::class => UlidGenerator::class,
+        MessageResolver::class => MessageResolverFromConfig::class,
+        WebHookServer::class => LaravelWebHookServer::class,
+    ];
 
     /**
      * Bootstrap any application services.
