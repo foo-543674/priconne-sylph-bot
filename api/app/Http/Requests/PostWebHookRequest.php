83b76f6d0925f012413453011d60d41a1c831f94
foo <foo.avoutar7654@gmail.com>
2021-09-08 11:48:35 +0000

    [update]バリデーションエラーのフォーマット変更

--- api/app/Http/Requests/PostWebHookRequest.php
+++ api/app/Http/Requests/PostWebHookRequest.php
@@ -25,7 +25,7 @@ class PostWebHookRequest extends FormRequest
     protected function failedValidation(Validator $validator)
     {
         $res = response()->json(
-            $validator->errors()->first()[0],
+            $validator->errors()->first(),
             400
         );
         throw new HttpResponseException($res);
