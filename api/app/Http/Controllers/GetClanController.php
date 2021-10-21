0516b70d38d98df6e04366189135e85ab488cf89
foo <foo.avoutar7654@gmail.com>
2021-10-21 15:16:23 +0000

    [update]クラン取得エンドポイントにパラメータ追加

--- api/app/Http/Controllers/GetClanController.php
+++ api/app/Http/Controllers/GetClanController.php
@@ -18,6 +18,16 @@ class GetClanController extends Controller
                 ->when($request->has('name'), function (Builder $query) use ($request) {
                     $query->where('name', $request->query('name', ''));
                 })
+                ->when($request->has('discord_user_id'), function (Builder $query) use ($request) {
+                    $query->whereHas('members', function (Builder $query) use ($request) {
+                        $query->where('discord_user_id', $request->query('discord_user_id'));
+                    });
+                })
+                ->when($request->has('report_channel_id'), function (Builder $query) use ($request) {
+                    $query->whereHas('reportChannels', function (Builder $query) use ($request) {
+                        $query->where('discord_channel_id', $request->query('report_channel_id'));
+                    });
+                })
                 ->get()
                 ->map(fn (Clan $record) => [
                     'id' => $record->id,
