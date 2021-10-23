9429a95ee9ec2bbc5883f48a9de2732c1f964504
foo <foo.avoutar7654@gmail.com>
2021-10-23 04:27:33 +0000

    [update]ステータス取得エンドポイントのレスポンスに日付追加

--- api/app/Http/Controllers/GetActivityController.php
+++ api/app/Http/Controllers/GetActivityController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Activity;
 use App\Models\Clan;
+use App\Models\ClanBattleDate;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Sylph\Entities\CarryOver;
 use Sylph\Entities\Challenge;
@@ -17,6 +19,11 @@ class GetActivityController extends Controller
      */
     public function __invoke(string $discordMessageId, string $discordUserId)
     {
+        /** @var ClanBattleDate $date */
+        $date = ClanBattleDate::query()->whereHas("reportMessages", function(Builder $query) use($discordMessageId) {
+            $query->where("discord_message_id", $discordMessageId);
+        })->firstOrFail();
+
         $activities = Activity::query()
             ->whereHas('actedDate', function (Builder $query) use ($discordMessageId) {
                 $query->whereHas('reportMessages', function (Builder $query) use ($discordMessageId) {
@@ -29,6 +36,7 @@ class GetActivityController extends Controller
             ->get();
 
         return response()->json([
+            "date" => $date->date_value,
             Challenge::getTypeName() => $activities->where('type', Challenge::getTypeName())->count(),
             CarryOver::getTypeName() => $activities->where('type', CarryOver::getTypeName())->count(),
             TaskKill::getTypeName() => $activities->where('type', TaskKill::getTypeName())->count(),
