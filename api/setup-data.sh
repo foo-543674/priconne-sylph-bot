8cc7d435906537f6014b5ed8f9bb3364a80f7420
Amane Nishi <amane.nishi@nulab.com>
2022-01-29 03:51:45 +0000

    [chore]ローカル環境のセットアップでAPIキーを.bashrcに書き込むように変更

--- api/setup-data.sh
+++ api/setup-data.sh
@@ -2,4 +2,4 @@ php artisan migrate
 KEY_NAME=api
 GENERATE_KEY_RESULT=`php artisan apikey:generate $KEY_NAME`
 KEY=`echo $GENERATE_KEY_RESULT | sed -r "s/API key created Name: .+ Key: (.+)/\1/"`
-echo $KEY
+echo "export API_KEY=$KEY" >> ~/.bashrc
