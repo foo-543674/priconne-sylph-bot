b4efa13302b634577048d01f76a12c4f6d1997db
Amane Nishi <amane.nishi@nulab.com>
2022-08-18 05:14:31 +0000

    [update]display boss questionaire other selection

--- bot-js/src/entities/BossQuestionnaire.ts
+++ bot-js/src/entities/BossQuestionnaire.ts
@@ -7,50 +7,70 @@ import { BossStamp } from "./BossStamp";
 type BossResolver = BossNumber | BossStamp;
 
 export class BossQuestionnaire {
-    public constructor(public readonly messageId: string, private readonly phraseRepository: PhraseRepository) {
-        this.result = bossNumbers
-            .map((bossNumber) => ({ [bossNumber]: [] }))
-            .reduce((prop, acc) => ({ ...acc, ...prop }));
-    }
+    public constructor(public readonly messageId: string, private readonly phraseRepository: PhraseRepository) {}
 
-    private readonly result: { [bossNumber: string]: GuildMember[] };
+    private answers: { [key in BossNumber]: GuildMember[] } = {
+        1: [],
+        2: [],
+        3: [],
+        4: [],
+        5: []
+    };
+    private index: { [key: string]: BossNumber[] } = {};
 
-    public add(bossNumber: BossResolver, member: GuildMember) {
+    public add(bossNumber: BossNumber, member: GuildMember): void;
+    public add(bossNumber: BossStamp, member: GuildMember): void;
+    public add(bossNumber: BossResolver, member: GuildMember): void {
         if (bossNumber instanceof BossStamp) {
-            this.result[bossNumber.number] = this.result[bossNumber.number].concat(member);
+            this.add(bossNumber.number, member);
         } else {
-            this.result[bossNumber] = this.result[bossNumber].concat(member);
+            this.answers[bossNumber] = this.answers[bossNumber].concat(member);
+            if (member.id in this.index) {
+                this.index[member.id] = this.index[member.id].concat(bossNumber).sort();
+            } else {
+                this.index[member.id] = [bossNumber];
+            }
         }
     }
 
-    public remove(bossNumber: BossResolver, member: GuildMember) {
+    public remove(bossNumber: BossNumber, member: GuildMember): void;
+    public remove(bossNumber: BossStamp, member: GuildMember): void;
+    public remove(bossNumber: BossResolver, member: GuildMember): void {
         if (bossNumber instanceof BossStamp) {
-            this.result[bossNumber.number] = this.result[bossNumber.number].filter((m) => m.id !== member.id);
+            this.remove(bossNumber.number, member);
         } else {
-            this.result[bossNumber] = this.result[bossNumber].filter((m) => m.id !== member.id);
+            this.answers[bossNumber] = this.answers[bossNumber].filter((m) => m.id !== member.id);
+            this.index[member.id] = this.index[member.id].filter((b) => b !== bossNumber);
+            if (this.index[member.id].length <= 0) {
+                delete this.index[member.id];
+            }
         }
     }
 
+    public get(bossNumber: BossNumber): GuildMember[];
+    public get(bossNumber: BossStamp): GuildMember[];
     public get(bossNumber: BossResolver): GuildMember[] {
         if (bossNumber instanceof BossStamp) {
-            return this.result[bossNumber.number];
+            return this.get(bossNumber.number);
         } else {
-            return this.result[bossNumber];
+            return this.answers[bossNumber];
         }
     }
 
     public generateEmbed(): EmbedFieldData[] {
-        return bossNumbers
-            .map((bossNumber) => new BossStamp(bossNumber, this.phraseRepository))
-            .map((bossStamp) => ({
-                name: `${bossStamp.value}${this.phraseRepository.get(PhraseKey.boss())}`,
-                value:
-                    this.result[bossStamp.number].length <= 0
-                        ? this.phraseRepository.get(PhraseKey.noChallengerMessage())
-                        : this.result[bossStamp.number]
-                              .map((member) => member.nickname ?? member.user.username)
-                              .join("\n"),
-                inline: true
-            }));
+        const createDisplayRow = (member: GuildMember, bossNumber: BossNumber) => {
+            const otherBossNumbers = this.index[member.id].filter((b) => b !== bossNumber);
+            const otherBossNumbersDisplay = otherBossNumbers.length > 0 ? `**(${otherBossNumbers.join()})**` : "";
+            return `${otherBossNumbersDisplay} ${member.displayName}`;
+        };
+
+        return bossNumbers.map((b) => ({
+            name: `${this.phraseRepository.get(PhraseKey.bossStamp(b))}${this.phraseRepository.get(PhraseKey.boss())}`,
+            value:
+                this.answers[b].length > 0
+                    ? this.answers[b].map((m) => createDisplayRow(m, b)).join("\n")
+                    : this.phraseRepository.get(PhraseKey.noChallengerMessage()),
+            inline: true
+        }));
     }
 }
