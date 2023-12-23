9bc215b440230b5ef3f05bdb93c852c026ff7379
Amane Nishi <amane.nishi@nulab.com>
2023-12-23 07:37:34 +0000

    [chore]local API_KEY for container was not used

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -13,5 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        if(config('app.env') === 'local') {
+            $this->call([
+                DefaultApiKeySeeder::class
+            ]);
+        }
     }
 }
